5c5434b7d339697e93a33a2ea44f1ccc3eee8063
bidgitty <66465944+bidgitty@users.noreply.github.com>
2020-10-30 07:09:58 -0700

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -3,9 +3,9 @@ def call(String project, String hubUser) {
     sh "docker tag ${hubUser}/${project} ${hubUser}/${project}:${ImageTag}"
     sh "docker tag ${hubUser}/${project} ${hubUser}/${project}:latest"
     withCredentials([usernamePassword(
-            credentialsId: "docker",
-            usernameVariable: "USER",
-            passwordVariable: "PASS"
+            credentialsId: 'docker',
+            usernameVariable: 'USER',
+            passwordVariable: 'PASS'
     )]) {
         sh "docker login -u '$USER' -p '$PASS'"
     }
